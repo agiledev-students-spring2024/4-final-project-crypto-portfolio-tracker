1d7c587ed3551d9c9a28d88930b7f63d1fc72c9f
reyhanquayum <rrq2003@nyu.edu>
2024-03-05 18:31:33 -0500

    aa

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -2,19 +2,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
-
+/**TODO:
+ * get hamburger icon, iconfify everything, make this actually look good then we chillin
+ */
 const Header = props => {
     return (
       <header className="Header-header">
-        <nav className="Header-navbar">
-          <ul className="nav-links">
+        <nav className="Header-navbar w-full">
+          <ul className="nav-links" >
             <li className="nav-item">
               <Link to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/messages">Messages</Link>
+              <Link to="/login">Login</Link>
             </li>
-              <li className="nav-item"><Link to="/about">About</Link>
+            <li className="nav-item">
+              <Link to="/settings">Settings</Link>
+            </li>
+              <li className="nav-item">
+                <Link to="/about">About</Link>
             </li>
           </ul>
         </nav>
