e9fb814b1d57843d4fa5ebb8f6f98c6fb013674d
reyhanquayum <rrq2003@nyu.edu>
2024-03-06 14:19:27 -0500

    landing done

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import './styles.css';
 import Header from './Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBitcoinSign} from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 const Home = (props) => {
     return (
       <div className="flex flex-col min-h-screen bg-dark-blue text-white">
         <Header />
-    
         <div className="flex flex-col items-center px-6 py-12">
-          <h1 className="text-4xl font-bold mb-4">Jump start your crypto portfolio </h1>
+          <h1 className="text-4xl font-bold mb-4">
+          Jump start your crypto portfolio </h1>
           <p className="text-lg mb-8">CryptoTracker is the easiest place to track your cryptos in one place. Sign up and get started today.</p>
           <form className="w-full max-w-md">
             <div className="flex flex-col items-stretch mb-6">
@@ -20,6 +18,7 @@ const Home = (props) => {
                 placeholder="Email address"
                 className="px-4 py-3 mb-4 rounded-md text-black"
               />
+              
               <button 
               className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-3 rounded-md transition duration-300"
               >
