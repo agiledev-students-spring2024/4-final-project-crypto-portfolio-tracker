8a3d15b2bd86118ddea42e48c0a49c537df8310f
reyhanquayum <rrq2003@nyu.edu>
2024-04-01 16:18:15 -0400

    formatted w prettier

--- back-end/app.js
+++ back-end/app.js
@@ -9,49 +9,48 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/login", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const data = {
-        status: "login accepted",
-        message: "login successful",
-        username: username,
-        password: password,
-    };
+  const data = {
+    status: "login accepted",
+    message: "login successful",
+    username: username,
+    password: password,
+  };
 
-    res.json(data);
- });
+  res.json(data);
+});
 
- app.post("/api/forgot_password", async (req, res) => {
-    const email = req.body.email;
+app.post("/api/forgot_password", async (req, res) => {
+  const email = req.body.email;
 
-    const data = {
-        status: "Email accepted",
-        message: "Email has been sent",
-        email: email,
-    };
-    
-    res.json(data);
- });
+  const data = {
+    status: "Email accepted",
+    message: "Email has been sent",
+    email: email,
+  };
 
- app.post("/api/register", async (req, res) => {
-    const name = req.body.name;
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+  res.json(data);
+});
 
-    const data = {
-        status: "User Registration successful",
-        message: "User has been registered",
-        name: name,
-        username: username,
-        email: email,
-        password: password,
-    };
-    
-    res.json(data);
- });
+app.post("/api/register", async (req, res) => {
+  const name = req.body.name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
+  const data = {
+    status: "User Registration successful",
+    message: "User has been registered",
+    name: name,
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  res.json(data);
+});
 
 app.get("/api/news", (req, res, next) => {
   const options = {
@@ -71,19 +70,21 @@ app.get("/api/news", (req, res, next) => {
 
 // Portfolio Routes
 
-  // get portfolio data from the database or wherever it's stored this is just temporary
-  const portfoliosData = [
-    // more portfolios will be added
+// get portfolio data from the database or wherever it's stored this is just temporary
+const portfoliosData = [
+  // more portfolios will be added
 ];
 
 // function to get the current Bitcoin price in USD
 async function getCurrentBitcoinPrice() {
   try {
-    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    );
     return response.data.bitcoin.usd;
   } catch (error) {
-    console.error('Error fetching Bitcoin price:', error);
-    return null; 
+    console.error("Error fetching Bitcoin price:", error);
+    return null;
   }
 }
 
@@ -91,39 +92,44 @@ app.get("/api/portfolios", async (req, res) => {
   try {
     const bitcoinPriceUSD = await getCurrentBitcoinPrice();
     if (!bitcoinPriceUSD) {
-      throw new Error('Failed to fetch Bitcoin price');
+      throw new Error("Failed to fetch Bitcoin price");
     }
 
-    const updatedPortfolios = await Promise.all(portfoliosData.map(async (portfolio) => {
-      let { platformId, address } = portfolio;
+    const updatedPortfolios = await Promise.all(
+      portfoliosData.map(async (portfolio) => {
+        let { platformId, address } = portfolio;
 
-      if (platformId !== 'bitcoin') {
-        return portfolio; // skip non-Bitcoin portfolios for now we can add ETH ADA and others here later
-      }
+        if (platformId !== "bitcoin") {
+          return portfolio; // skip non-Bitcoin portfolios for now we can add ETH ADA and others here later
+        }
 
-      // get the balance from the Mempool API
-      const url = `https://mempool.space/api/address/${address}`;
-      const response = await axios.get(url);
-      const { chain_stats, mempool_stats } = response.data;
+        // get the balance from the Mempool API
+        const url = `https://mempool.space/api/address/${address}`;
+        const response = await axios.get(url);
+        const { chain_stats, mempool_stats } = response.data;
 
-      let confirmedBalance = chain_stats.funded_txo_sum - chain_stats.spent_txo_sum;
-      let unconfirmedBalance = mempool_stats.funded_txo_sum - mempool_stats.spent_txo_sum;
-      let totalBalanceSatoshis = confirmedBalance + unconfirmedBalance;
-      let totalBalanceBTC = totalBalanceSatoshis / 100000000; // convert satoshis to Bitcoin
+        let confirmedBalance =
+          chain_stats.funded_txo_sum - chain_stats.spent_txo_sum;
+        let unconfirmedBalance =
+          mempool_stats.funded_txo_sum - mempool_stats.spent_txo_sum;
+        let totalBalanceSatoshis = confirmedBalance + unconfirmedBalance;
+        let totalBalanceBTC = totalBalanceSatoshis / 100000000; // convert satoshis to Bitcoin
 
-      let balanceUSD = totalBalanceBTC * bitcoinPriceUSD;
+        let balanceUSD = totalBalanceBTC * bitcoinPriceUSD;
 
-      // return the portfolio with the balance in USD
-      return { ...portfolio, balance: `$${balanceUSD.toFixed(2)}` };
-    }));
+        // return the portfolio with the balance in USD
+        return { ...portfolio, balance: `$${balanceUSD.toFixed(2)}` };
+      })
+    );
 
     res.json(updatedPortfolios);
   } catch (error) {
-    console.error('Error fetching portfolio data with balance:', error);
-    res.status(500).json({ message: 'Error fetching portfolio data with balance' });
+    console.error("Error fetching portfolio data with balance:", error);
+    res
+      .status(500)
+      .json({ message: "Error fetching portfolio data with balance" });
   }
 });
-
 
 app.post("/api/addWallet", async (req, res) => {
   const { name, address, balance } = req.body;
@@ -132,28 +138,27 @@ app.post("/api/addWallet", async (req, res) => {
   const newPortfolio = {
     id: `portfolio-${portfoliosData.length + 1}`, // ID generation we can change later when we integrate database
     name,
-    platformId: 'bitcoin',
+    platformId: "bitcoin",
     address,
-    balance
+    balance,
   };
 
   portfoliosData.push(newPortfolio);
 
   res.json({
     portfolios: portfoliosData,
-    message: `Address ${address} received and processed.`
+    message: `Address ${address} received and processed.`,
   });
 });
 
 app.delete("/api/deleteWallet/:id", async (req, res) => {
   const { id } = req.params;
-  console.log(`DELETE request to /api/deleteWallet with ID ${id}`)
+  console.log(`DELETE request to /api/deleteWallet with ID ${id}`);
   // find wallet by ID and delete it -- neds to be implemented
 
   //for now just send a message
-  res.json({message: `Wallet with ID ${id} deleted.`})
-})
-
+  res.json({ message: `Wallet with ID ${id} deleted.` });
+});
 
 //For CryptoList API - Route handler for GET requests to the '/api/coins' endpoint
 
